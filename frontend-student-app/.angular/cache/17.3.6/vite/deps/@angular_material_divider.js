d48f354f234313827349ef901af4769b6939fd31
1Peripheral <codels@protonmail.com>
2024-05-02 18:18:33 +0100

    frontend

---
+++ frontend-student-app/.angular/cache/17.3.6/vite/deps/@angular_material_divider.js
@@ -0,0 +1,12 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-HYZ5QHQJ.js";
+import "./chunk-RTKBEY2L.js";
+import "./chunk-FLOEYVWJ.js";
+import "./chunk-B4YSVMJT.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
+//# sourceMappingURL=@angular_material_divider.js.map
